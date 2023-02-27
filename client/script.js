c8af78f4af5ee406580f46c7656e93b9f7ea2d8b
nika110 <nika.asatiani.1@btu.edu.ge>
2023-02-27 23:24:13 +0400

    changed IP

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-dm3e.onrender.com/', {
+    const response = await fetch('https://216.24.57.253:5000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
