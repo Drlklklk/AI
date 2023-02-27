e9de6539acb900f937253eeb1e9b53ec81269c6a
nika110 <nika.asatiani.1@btu.edu.ge>
2023-02-28 00:13:47 +0400

    Deployment not working

--- client/script.js
+++ client/script.js
@@ -85,6 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
+    console.log('z')
 
     const response = await fetch('https://76.76.21.142:5010/', {
         method: 'POST',
